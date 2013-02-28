44d437a5d37b1f18a2e0aca3206658c5146dfe38
Patrick Herrmann <patrickwherrmann@gmail.com>
2013-02-27 18:24:38 -0800

    Stub ByIOS, update project

--- BumblebeeIOS/ByIOS.cs
+++ BumblebeeIOS/ByIOS.cs
@@ -1,7 +1,12 @@
-﻿namespace BumblebeeIOS
+﻿using OpenQA.Selenium;
+
+namespace BumblebeeIOS
 {
     public class ByIOS
     {
-
+        public static By SomeMethod()
+        {
+            return By.XPath("blah blah blah");
+        }
     }
 }
