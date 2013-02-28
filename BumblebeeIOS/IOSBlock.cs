0a2554056fb5cc04cecdadb37183043fcc932933
Patrick Herrmann <patrickwherrmann@gmail.com>
2013-02-27 16:44:10 -0800

    Initial commit

---
+++ BumblebeeIOS/IOSBlock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bumblebee.Implementation;
+using Bumblebee.Setup;
+using OpenQA.Selenium;
+
+namespace BumblebeeIOS
+{
+    public class IOSBlock : Block
+    {
+        public IOSBlock(Session session) : base(session)
+        {
+            Tag = Session.Driver.FindElement(By.ClassName("UIAWindow"));
+        }
+    }
+}
