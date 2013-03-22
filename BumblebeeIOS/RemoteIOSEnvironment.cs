129788eb0bd3b6863ee2d2f36267575d0e43c4b0
John Hammerlund <john.hammerlund@mindbodyonline.com>
2013-03-21 17:10:28 -0700

    Add convenience to environment construction, allow JSON map

--- BumblebeeIOS/RemoteIOSEnvironment.cs
+++ BumblebeeIOS/RemoteIOSEnvironment.cs
@@ -9,30 +9,59 @@ namespace BumblebeeIOS
     public class RemoteIOSEnvironment : IDriverEnvironment
     {
         private string _address;
-        private string _bundleName;
+        private Dictionary<string, object> _json; 
 
-        public RemoteIOSEnvironment(string address, string bundleName)
+
+        /// <summary>
+        /// Constructs a Bumblebee iOS test environment with minimum requirements.
+        /// </summary>
+        /// <param name="address">Grid node session address</param>
+        /// <param name="bundleName">Name of the iOS Application</param>
+        /// <param name="isIphone">True if using iPhone, false if using iPad</param>
+        public RemoteIOSEnvironment(string address, string bundleName, bool isIphone = true) 
+            : this(address, bundleName, "1.0")
+        {
+        }
+
+        /// <summary>
+        /// Constructs a Bumblebee iOS test environment with basic requirements.
+        /// </summary>
+        /// <param name="address">Grid node session address</param>
+        /// <param name="bundleName">Name of the iOS Application</param>
+        /// <param name="version">Specific version of the iOS Application</param>
+        /// <param name="isIphone">True if using iPhone, false if using iPad</param>
+        public RemoteIOSEnvironment(string address, string bundleName, string version, bool isIphone = true)
         {
             _address = address;
-            _bundleName = bundleName;
+
+            _json = new Dictionary<string, object>
+                           {
+                               {"simulator", false},
+                               {"CFBundleName", bundleName},
+                               {"locale", "en_GB"},
+                               {"variation", "Regular"},
+                               {"timeHack", false},
+                               {"device", isIphone ? "iphone" : "ipad"},
+                               {"CFBundleVersion", version},
+                               {"language", "en"}
+                           };
+        }
+
+        /// <summary>
+        /// Constructs a Bumblebee iOS test environment using user-defined DesiredCapabilities
+        /// <para>See http://ios-driver.github.com/ios-driver-beta/native.html#params </para>
+        /// </summary>
+        /// <param name="jsonMap">JSON required to fulfill Capabilities for the grid protocol.</param>
+        public RemoteIOSEnvironment(string address, Dictionary<string, object> jsonMap)
+        {
+            _address = address;
+            _json = jsonMap;
         }
 
         public IWebDriver CreateWebDriver()
         {
-            //var test = IOSCapabilities.Iphone(_bundleName);
-            var json = new Dictionary<string, object>
-                           {
-                               {"simulator", false},
-                               {"CFBundleName", _bundleName},
-                               {"locale", "en_GB"},
-                               {"variation", "Regular"},
-                               {"timeHack", false},
-                               {"device", "iphone"},
-                               {"CFBundleVersion", "1.0"},
-                               {"language", "en"}
-                           };
-            var raw = new DesiredCapabilities(json);
-            return new RemoteWebDriver(new Uri(_address), raw);
+            return new RemoteWebDriver(new Uri(_address), new DesiredCapabilities(_json));
         }
+
     }
 }
